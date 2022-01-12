93eff4e15c60d2dee046f78405eaad53eba7fc24
Meenal-Poladia <meenal.poladia94@gmail.com>
2022-01-12 17:34:54 +0530

    Adding ErrorModal component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,6 +2,7 @@ import Card from "../UI/Card";
 import classes from "./Adduser.module.css";
 import Button from "../UI/Button";
 import {useState} from "react";
+import ErrorModal from "../UI/ErrorModal";
 
 const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState("");
@@ -29,24 +30,28 @@ const AddUser = (props) => {
     }
 
     return (
-        <Card className={classes.input}>
-            <form onSubmit={addUserHandler}>
-                <label htmlFor="username">Username</label>
-                <input
-                    type="text"
-                    id="username"
-                    value={enteredUsername}
-                    onChange={usernameChangeHandler}/>
-                <label htmlFor="age">Age</label>
-                <input
-                    type="number"
-                    id="age"
-                    value={enteredAge}
-                    onChange={ageChangeHandler}
-                />
-                <Button type="submit">Add User</Button>
-            </form>
-        </Card>
+        <div>
+            <ErrorModal title="An error occured" message="Something went wrong"/>
+            <Card className={classes.input}>
+                <form onSubmit={addUserHandler}>
+                    <label htmlFor="username">Username</label>
+                    <input
+                        type="text"
+                        id="username"
+                        value={enteredUsername}
+                        onChange={usernameChangeHandler}/>
+                    <label htmlFor="age">Age</label>
+                    <input
+                        type="number"
+                        id="age"
+                        value={enteredAge}
+                        onChange={ageChangeHandler}
+                    />
+                    <Button type="submit">Add User</Button>
+                </form>
+            </Card>
+        </div>
+
   )
 }
 export default AddUser;
